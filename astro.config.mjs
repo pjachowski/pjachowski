1895e614b32b067d98cd92d0dcabb64980590c25
Patryk Jachowski <patryk.jachowski@gmail.com>
2026-02-13 16:02:24 +0100

    feat: add starlight theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,24 @@
+// @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "My delightful docs site",
-    }),
-  ],
+	integrations: [
+		starlight({
+			title: "My Docs",
+			social: [
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/withastro/starlight",
+				},
+			],
+			sidebar: [
+				{
+					label: "Technologie",
+					items: [{ label: "TypeScript", slug: "typescript" }],
+				},
+			],
+		}),
+	],
 });
